0819084f76f0992fdce6b677df19dd8f8ebfcb80
duduita <eduardomenezesm@msn.com>
2021-02-24 08:20:35 -0300

    Comment radar

--- src/components/RadarLayer/index.js
+++ src/components/RadarLayer/index.js
@@ -5,27 +5,38 @@ import { connect } from "react-redux";
 import axios from "axios";
 import "leaflet-velocity";
 
-var myLayerGroup = [];
+// Função para cálcular as horas passadas
+const RequestDate = (requestDate, j) => {
+  requestDate.setHours(requestDate.getHours() - 6 + j);
+  let year = requestDate.getFullYear();
+  let month = requestDate.getMonth();
+  let day = requestDate.getDate();
+  let hour = requestDate.getHours();
+  if (hour < 10) hour = `0${hour}`;
+  if (day < 10) day = `0${day}`;
+  if (month < 10) month = `0${month}`;
+  return `${year}${month}${day}${hour}`;
+};
+
 const RadarLayer = (props) => {
+  let myLayerGroup = [];
+  // Função que detecta o contexto, i.e., o mapa que carregara as layers
   const context = useLeafletContext();
   const container = context.map;
+  // Análogo ao ComponentDidMount e o ComponentWillMount
+  // Para saber mais: https://reactjs.org/docs/hooks-effect.html
   useEffect(() => {
+    // Condicional para carregar apenas uma vez
     if (myLayerGroup.length === 0) {
       let requestDate = new Date();
       for (let j = 1; j <= 6; j++) {
-        requestDate.setHours(requestDate.getHours() - 6 + j);
-        let year = requestDate.getFullYear();
-        let month = requestDate.getMonth();
-        let day = requestDate.getDate();
-        let hour = requestDate.getHours();
-        if (hour < 10) hour = `0${hour}`;
-        if (day < 10) day = `0${day}`;
-        if (month < 10) month = `0${month}`;
-        let requestHour = `${year}${month}${day}${hour}`;
+        let requestHour = RequestDate(requestDate, j);
         let url = `https://api-redemet.decea.mil.br/produtos/radar/maxcappi?api_key=gdkP7S0gy9sB4JsOLoYe34D52CGyrDzZK3xAWe80&data=${requestHour}`;
         myLayerGroup[j] = L.layerGroup();
+        // Requisição do georefenciamento e das imagens
         axios.get(url).then((res) => {
           res = res.data;
+          // Agrupando o conjunto de radares na mesma layer
           for (let i = 0; i < res.data.radar[0].length; i++) {
             var imageBounds = [
               [res.data.radar[0][i].lat_min, res.data.radar[0][i].lon_min],
@@ -34,18 +45,18 @@ const RadarLayer = (props) => {
             var imageUrl = res.data.radar[0][i].path;
             if (imageUrl != undefined) {
               var layer_radar = L.imageOverlay(imageUrl, imageBounds);
+              // Adicionando a layer ao meu LayerGroup
               myLayerGroup[j].addLayer(layer_radar);
             }
           }
         });
       }
     }
+    // Se cliente solicitou, adicionar ao container (mapa)
     if (props.radar) {
-      console.log(myLayerGroup[props.date]);
       container.addLayer(myLayerGroup[props.date]);
     }
     return () => {
-      console.log(container.hasLayer(myLayerGroup[props.date]));
       if (container.hasLayer(myLayerGroup[props.date]))
         container.removeLayer(myLayerGroup[props.date]);
     };
@@ -53,14 +64,12 @@ const RadarLayer = (props) => {
   return null;
 };
 
+// Mapeia os estados para propriedades (redux)
 const mapStateToProps = (state) => {
   return {
     radar: state.radar,
     date: state.date,
   };
 };
-const mapDispatchToProps = (dispatch) => {
-  return {};
-};
 
-export default connect(mapStateToProps, mapDispatchToProps)(RadarLayer);
+export default connect(mapStateToProps)(RadarLayer);
--- src/index.js
+++ src/index.js
@@ -6,8 +6,10 @@ import { createStore } from "redux";
 import reducer from "./store/reducer";
 import Map from "./components/Map";
 
+// Creating a provider to redux
 const store = createStore(reducer);
 
+// Render our Map with redux provider
 ReactDOM.render(
   <Provider store={store}>
     <Map />
